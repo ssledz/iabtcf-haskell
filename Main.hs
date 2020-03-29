989b50ef51fd5b75731f8dd3d8a1403e63e39c29
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-03-29 20:34:50 +0200

    playing with base64 lib

--- Main.hs
+++ Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base64 as Base64
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let helloHaskell = "Hello, Haskell !"
+    let str = C.pack helloHaskell
+    let strEnc = show $ Base64.encode str
+    putStrLn $ "base64.encoded(" ++ helloHaskell ++ ") = " ++ strEnc
